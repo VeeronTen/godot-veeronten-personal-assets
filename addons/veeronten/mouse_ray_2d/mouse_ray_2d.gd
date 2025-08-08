645d5aaa8ec6387c765718820248369326dbefa4
Veeron Ten <veeronten@gmail.com>
2025-08-08 23:00:53 +0200

    add MouseRay2D

---
+++ addons/veeronten/mouse_ray_2d/mouse_ray_2d.gd
@@ -0,0 +1,14 @@
+@icon("res://addons/veeronten/mouse_ray_2d/mouse_ray_2d_icon.svg")
+class_name MouseRay2D 
+extends RayCast2D
+## A ray to cast from and to the mouse position
+
+func cast() -> String:
+	_cast_assertions()
+	global_position = get_global_mouse_position()
+	force_raycast_update()
+	return get_collider().name if is_colliding() else ""
+		
+func _cast_assertions() -> void:
+	assert(target_position == Vector2.ZERO, "mouse offset have to be 0")
+	assert(hit_from_inside, "the ray length is 0, so it has to hit from inside")
