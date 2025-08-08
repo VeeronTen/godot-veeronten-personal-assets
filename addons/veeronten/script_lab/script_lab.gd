c2e26b1747a500b158f798d5e5a4a4a7b9a00ef4
Veeron Ten <veeronten@gmail.com>
2025-08-08 23:01:03 +0200

    add script lab

---
+++ addons/veeronten/script_lab/script_lab.gd
@@ -0,0 +1,10 @@
+@tool 
+extends EditorScript 
+
+func _process(delta: float) -> void: 
+	assert(false, "the lab have to be used only with func _run()")
+
+# Windows: Ctrl + Shift + X
+# Mac: TODO
+func _run():
+	pass
