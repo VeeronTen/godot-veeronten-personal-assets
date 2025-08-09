9e41ca2b94b5f1f9cda7f80be3f15b44478661dc
Veeron Ten <veeronten@gmail.com>
2025-08-09 22:18:37 +0300

    mouse ray 2d restrictions

--- personal_addons/mouse_ray_2d/mouse_ray_2d.gd
+++ personal_addons/mouse_ray_2d/mouse_ray_2d.gd
@@ -1,8 +1,21 @@
+@tool
 @icon("res://personal_addons/mouse_ray_2d/mouse_ray_2d_icon.svg")
 class_name MouseRay2D 
 extends RayCast2D
 ## A ray to cast from and to the mouse position
 
+var _restrictions = [
+	[func(): return target_position == Vector2.ZERO, "`target_position` as mouse offset have to be 0"],
+	[func(): return hit_from_inside, "the ray length is 0, so it has to `hit_from_inside`"]
+]
+
+func _get_configuration_warnings():
+	return _restrictions.filter(
+		func(restriction): return not restriction[0].call()
+	).map(
+		func(restriction): return restriction[1]
+	)
+	
 func cast() -> String:
 	_cast_assertions()
 	global_position = get_global_mouse_position()
@@ -10,5 +23,5 @@ func cast() -> String:
 	return get_collider().name if is_colliding() else ""
 		
 func _cast_assertions() -> void:
-	assert(target_position == Vector2.ZERO, "mouse offset have to be 0")
-	assert(hit_from_inside, "the ray length is 0, so it has to hit from inside")
+	for restriction in _restrictions:
+		assert(restriction[0].call(), restriction[1])
