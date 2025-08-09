030fb1b7aba7bb3d4e93e5972808faad9edf1fec
Veeron Ten <veeronten@gmail.com>
2025-08-09 15:38:49 +0200

    add assertion that the settings were found + print log if no project settings changed

--- addons/project_editor_settings/project_editor_settings_dock.gd
+++ addons/project_editor_settings/project_editor_settings_dock.gd
@@ -13,13 +13,17 @@ func _on_button_enbable_all_gdscript_warnings_pressed() -> void:
 	var warning_project_settings = ProjectSettings.get_property_list().filter(
 		func(setting): return PROJECT_SETTING_NAME_PATTERN_GDSCRIPT_WARNINGS in setting.name
 	)
+	assert(not warning_project_settings.is_empty(), "warning settings were not found!")
 	var ignored_warning_project_settings = warning_project_settings.filter(
 		func(warning_setting): 
 			var current_value = ProjectSettings.get_setting(warning_setting.name)
 			return current_value == 0 if current_value is int else false
 	)
-	for setting in ignored_warning_project_settings:
-		_set_project_setting_and_log(setting.name, 1)
+	if ignored_warning_project_settings.is_empty():
+		print("PROJECT all ", PROJECT_SETTING_NAME_PATTERN_GDSCRIPT_WARNINGS, " settings were set correctly already")
+	else:
+		for setting in ignored_warning_project_settings: 
+			_set_project_setting_and_log(setting.name, 1)
 	ProjectSettings.save()
 
 func _on_button_close_plugin_pressed() -> void:
